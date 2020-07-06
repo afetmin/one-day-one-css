633a123824233737345afff68ee4bb12a7b6765f
afetmin <kingpenguins@163.com>
2020-07-06 09:48:27 +0800

    fix格式

--- chinaVis/js/controller.js
+++ chinaVis/js/controller.js
@@ -5,14 +5,17 @@ function getTime() {
   //     $("#tim").html(time_str);
   //   },
   // });
-  let time = new Date()
-  let year = time.getFullYear()
-  let month = time.getMonth()+1 < 10 ? `0${time.getMonth()+1}` : time.getMonth()+1
-  let date = time.getDate() < 10 ? `0${time.getDate()}` : time.getDate()
-  let hour = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours()
-  let minutes = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
-  let seconds = time.getSeconds() < 10 ? `0${time.getSeconds()}` : time.getSeconds()
-  let time_str = `${year}年${month}月${date}日 ${hour}:${minutes}:${seconds}`
+  let time = new Date();
+  let year = time.getFullYear();
+  let month =
+    time.getMonth() + 1 < 10 ? `0${time.getMonth() + 1}` : time.getMonth() + 1;
+  let date = time.getDate() < 10 ? `0${time.getDate()}` : time.getDate();
+  let hour = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+  let minutes =
+    time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+  let seconds =
+    time.getSeconds() < 10 ? `0${time.getSeconds()}` : time.getSeconds();
+  let time_str = `${year}年${month}月${date}日 ${hour}:${minutes}:${seconds}`;
   $("#tim").html(time_str);
 }
 function get_c1_data() {
